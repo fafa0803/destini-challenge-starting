6c344b997be521ad301e5e16f417491885c557ac
Fabian <f.faulhaber@outlook.com>
2019-11-07 15:56:08 +0100

    Display first story in the App

--- lib/story.dart
+++ lib/story.dart
@@ -1,11 +1,11 @@
 class Story {
-  String _storyTitle;
-  String _choice1;
-  String _choice2;
+  String storyTitle;
+  String choice1;
+  String choice2;
 
   Story({String storyTitle, String choice1, String choice2}) {
-    this._storyTitle = storyTitle;
-    this._choice1 = choice1;
-    this._choice2 = choice2;
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
   }
 }
